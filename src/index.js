be75bc3a2bad993afc7e0617e154411958d7f566
Evgeny Timoshenko <timoshenko.evgeny@gmail.com>
2018-06-25 16:23:46 +0200

    wip: typeahead

--- src/index.js
+++ src/index.js
@@ -11,14 +11,10 @@ class Chip extends React.Component {
 
 class Menu extends React.Component {
   render() {
+    console.log("---", this.props);
     return (
       <ul className="menu">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>3</li>
-        <li>3</li>
-        <li>3</li>
+        {this.props.items.map(item => <li>{item}</li>)}
       </ul>
     );
   }
@@ -28,13 +24,20 @@ class Filter extends React.Component {
   state = {
     dropdownOffset: 0,
     active: false,
-    open: false
+    open: false,
+    activeIndex: null
   };
   render() {
     return (
-      <div className="filter__root" ref={that => (this._rootRef = that)}>
+      <div
+        className="filter__root"
+        ref={that => (this._rootRef = that)}
+      >
         <div className="filter__filter-icon">
-          <svg className="filter__filter-svg" viewBox="0 0 18 18">
+          <svg
+            className="filter__filter-svg"
+            viewBox="0 0 18 18"
+          >
             <path d="M2 4 h14v2H2V4z m2 4 h10v2H4V8z m2 4 h6v2H6v-2z" />
           </svg>
         </div>
@@ -49,6 +52,7 @@ class Filter extends React.Component {
           placeholder="Filter..."
           onFocus={this.handleInputFocus}
           onBlur={this.handleInputBlur}
+          onKeyDown={this.handleKeyDown}
         />
         {this.state.active && (
           <div
@@ -57,7 +61,10 @@ class Filter extends React.Component {
               left: this.state.dropdownOffset
             }}
           >
-            <Menu />
+            <Menu
+              activeIndex={this.state.activeIndex}
+              q={[1, 2, 3, 4, 5]}
+            />
           </div>
         )}
       </div>
@@ -67,20 +74,32 @@ class Filter extends React.Component {
   handleInputFocus = e => {
     const x = this._rootRef.getBoundingClientRect().x;
     this.setState({
-      dropdownOffset: e.target.getBoundingClientRect().x - x,
+      dropdownOffset:
+        e.target.getBoundingClientRect().x - x,
       active: true
     });
   };
   handleInputBlur = e => {
     // this.setState({ active: false });
   };
+  handleKeyDown = e => {
+    if (e.keyCode === 40) {
+      this.setState(state =>
+        Object.assign(
+          {},
+          state,
+          (state.activeIndex || 0) + 1
+        )
+      );
+    }
+    e.persist();
+    console.log(e);
+  };
 }
 
 function App() {
   return (
     <div>
-      <Menu />
-      <Chip />
       <Filter />
     </div>
   );
